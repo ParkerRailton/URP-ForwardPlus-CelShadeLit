21cde04c04f860276aa4a472be6dc953347aa274
Parker Railton <parker.railton@gmail.com>
2026-03-17 12:21:29 -0400

    additional light shadow support + color capping

--- CelShadeLit/CustomNodes/LightingCelShaded.hlsl
+++ CelShadeLit/CustomNodes/LightingCelShaded.hlsl
@@ -8,98 +8,102 @@
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl" 
 
 #ifndef SHADERGRAPH_PREVIEW
-struct EdgeConstants {
+struct EdgeConstants
+{
 
-   float diffuse;
-   float specular;
-   float rim;
-   float distanceAttenuation;
-   float shadowAttenuation;
-
-};
-
-struct SurfaceVariables {
-
-   float smoothness;
-   float shininess;
-   
-   float rimStrength;
-   float rimAmount;
-   float rimThreshold;
-   
-   float3 normal;
-   float3 view;
-   float3 clipPosition;
-
-   EdgeConstants ec;
+    float diffuse;
+    float specular;
+    float rim;
+    float distanceAttenuation;
+    float shadowAttenuation;
 
 };
 
-float3 CalculateCelShading(Light l, SurfaceVariables s) {
-   float attenuation = 
-      smoothstep(0.0f, s.ec.distanceAttenuation, l.distanceAttenuation) * 
+struct SurfaceVariables
+{
+
+    float smoothness;
+    float shininess;
+   
+    float rimStrength;
+    float rimAmount;
+    float rimThreshold;
+   
+    float3 normal;
+    float3 view;
+    float3 clipPosition;
+
+    EdgeConstants ec;
+
+};
+
+float3 CalculateCelShading(Light l, SurfaceVariables s, half shadowValue)
+{
+    float attenuation =
+      smoothstep(0.0f, s.ec.distanceAttenuation, l.distanceAttenuation) *
       smoothstep(0.0f, s.ec.shadowAttenuation, l.shadowAttenuation);
 
-   float diffuse = saturate(dot(s.normal, l.direction));
-   diffuse *= attenuation;
+    float diffuse = saturate(dot(s.normal, l.direction));
+    diffuse *= attenuation;
 
-   float3 h = SafeNormalize(l.direction + s.view);
-   float specular = saturate(dot(s.normal, h));
-   specular = pow(specular, s.shininess);
-   specular *= diffuse;
+    float3 h = SafeNormalize(l.direction + s.view);
+    float specular = saturate(dot(s.normal, h));
+    specular = pow(specular, s.shininess);
+    specular *= diffuse;
 
-   float rim = 1 - dot(s.view, s.normal);
-   rim *= pow(diffuse, s.rimThreshold);
+    float rim = 1 - dot(s.view, s.normal);
+    rim *= pow(diffuse, s.rimThreshold);
 
-   diffuse = smoothstep(0.0f, s.ec.diffuse, diffuse);
-   specular = s.smoothness * smoothstep(0.005f, 
+    diffuse = smoothstep(0.0f, s.ec.diffuse, diffuse);
+    specular = s.smoothness * smoothstep(0.005f,
       0.005f + s.ec.specular * s.smoothness, specular);
-   rim = s.rimStrength * smoothstep(
-      s.rimAmount - 0.5f * s.ec.rim, 
-      s.rimAmount + 0.5f * s.ec.rim, 
+    rim = s.rimStrength * smoothstep(
+      s.rimAmount - 0.5f * s.ec.rim,
+      s.rimAmount + 0.5f * s.ec.rim,
       rim
    );
 
-   return l.color * (diffuse + max(specular, rim));
+    return (l.color * (diffuse + max(specular, rim))) * shadowValue;
 }
 #endif
 
-void LightingCelShaded_float(float Smoothness, 
-      float RimStrength, float RimAmount, float RimThreshold, float3 ClipPos, 
+void LightingCelShaded_float(float Smoothness,
+      float RimStrength, float RimAmount, float RimThreshold, float3 ClipPos,
       float3 Position, float3 Normal, float3 View, float EdgeDiffuse,
       float EdgeSpecular, float EdgeDistanceAttenuation,
-      float EdgeShadowAttenuation, float EdgeRim, out float3 Color) {
+      float EdgeShadowAttenuation, float EdgeRim, bool CapColorValue, float CapThresh, out float3 Color)
+{
 
 #if defined(SHADERGRAPH_PREVIEW)
    Color = half3(0.5f, 0.5f, 0.5f);
 #else
-   SurfaceVariables s;
-   s.smoothness = Smoothness;
-   s.shininess = exp2(10 * Smoothness + 1);
-   s.rimStrength = RimStrength;
-   s.rimAmount = RimAmount;
-   s.rimThreshold = RimThreshold;
-   s.normal = SafeNormalize(Normal);
-   s.view = SafeNormalize(View);
-   s.clipPosition = ClipPos;
-   s.ec.diffuse = EdgeDiffuse;
-   s.ec.specular = EdgeSpecular;
-   s.ec.distanceAttenuation = EdgeDistanceAttenuation;
-   s.ec.shadowAttenuation = EdgeShadowAttenuation;
-   s.ec.rim = EdgeRim;
+    SurfaceVariables s;
+    s.smoothness = Smoothness;
+    s.shininess = exp2(10 * Smoothness + 1);
+    s.rimStrength = RimStrength;
+    s.rimAmount = RimAmount;
+    s.rimThreshold = RimThreshold;
+    s.normal = SafeNormalize(Normal);
+    s.view = SafeNormalize(View);
+    s.clipPosition = ClipPos;
+    s.ec.diffuse = EdgeDiffuse;
+    s.ec.specular = EdgeSpecular;
+    s.ec.distanceAttenuation = EdgeDistanceAttenuation;
+    s.ec.shadowAttenuation = EdgeShadowAttenuation;
+    s.ec.rim = EdgeRim;
 
 #if SHADOWS_SCREEN
    float4 clipPos = TransformWorldToHClip(Position);
    float4 shadowCoord = ComputeScreenPos(clipPos);
 #else
-   float4 shadowCoord = TransformWorldToShadowCoord(Position);
+    float4 shadowCoord = TransformWorldToShadowCoord(Position);
 #endif
     
     
     Color = 0;
     
     Light light = GetMainLight(shadowCoord);
-    Color = CalculateCelShading(light, s);
+    Color = CalculateCelShading(light, s, 1);
 
     
 
@@ -118,10 +122,23 @@ inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(screenPos);
     
 LIGHT_LOOP_BEGIN(pixelLightCount)
     Light addLight = GetAdditionalLight(lightIndex, Position);
-    Color += CalculateCelShading(addLight, s);
+    half shadowValue = AdditionalLightRealtimeShadow(lightIndex, Position);
+    Color += CalculateCelShading(addLight, s, shadowValue);
 LIGHT_LOOP_END
 
 #endif
+    if (CapColorValue)
+    {
+        float cMagnitude = length(Color);
+        if (cMagnitude > CapThresh)
+        {
+            float cMod = CapThresh / cMagnitude;
+            Color.x *= cMod;
+            Color.y *= cMod;
+            Color.z *= cMod;
+        }
+    }
+    
    
 #endif
 }

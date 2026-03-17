e37a6689456d8bd6b704bdabecf5b47d4b58220b
Parker Railton <parker.railton@gmail.com>
2026-03-17 11:12:47 -0400

    Updated files to support Forward+

--- CelShadeLit/CustomNodes/LightingCelShaded.hlsl
+++ CelShadeLit/CustomNodes/LightingCelShaded.hlsl
@@ -1,6 +1,12 @@
 ﻿#ifndef LIGHTING_CEL_SHADED_INCLUDED
 #define LIGHTING_CEL_SHADED_INCLUDED
 
+#pragma multi_compile _ _FORWARD_PLUS
+#pragma multi_compile_fragment _ _ADDITIONAL_LIGHTS
+#pragma multi_compile _ _ADDITIONAL_LIGHT_SHADOWS
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl" 
+
 #ifndef SHADERGRAPH_PREVIEW
 struct EdgeConstants {
 
@@ -23,6 +29,7 @@ struct SurfaceVariables {
    
    float3 normal;
    float3 view;
+   float3 clipPosition;
 
    EdgeConstants ec;
 
@@ -58,7 +65,7 @@ float3 CalculateCelShading(Light l, SurfaceVariables s) {
 #endif
 
 void LightingCelShaded_float(float Smoothness, 
-      float RimStrength, float RimAmount, float RimThreshold, 
+      float RimStrength, float RimAmount, float RimThreshold, float3 ClipPos, 
       float3 Position, float3 Normal, float3 View, float EdgeDiffuse,
       float EdgeSpecular, float EdgeDistanceAttenuation,
       float EdgeShadowAttenuation, float EdgeRim, out float3 Color) {
@@ -72,8 +79,9 @@ void LightingCelShaded_float(float Smoothness,
    s.rimStrength = RimStrength;
    s.rimAmount = RimAmount;
    s.rimThreshold = RimThreshold;
-   s.normal = normalize(Normal);
+   s.normal = SafeNormalize(Normal);
    s.view = SafeNormalize(View);
+   s.clipPosition = ClipPos;
    s.ec.diffuse = EdgeDiffuse;
    s.ec.specular = EdgeSpecular;
    s.ec.distanceAttenuation = EdgeDistanceAttenuation;
@@ -86,15 +94,34 @@ void LightingCelShaded_float(float Smoothness,
 #else
    float4 shadowCoord = TransformWorldToShadowCoord(Position);
 #endif
+    
+    
+    Color = 0;
+    
+    Light light = GetMainLight(shadowCoord);
+    Color = CalculateCelShading(light, s);
 
-   Light light = GetMainLight(shadowCoord);
-   Color = CalculateCelShading(light, s);
+    
 
-   int pixelLightCount = GetAdditionalLightsCount();
-   for (int i = 0; i < pixelLightCount; i++) {
-      light = GetAdditionalLight(i, Position, 1);
-      Color += CalculateCelShading(light, s);
-   }
+#if defined(_ADDITIONAL_LIGHTS)
+
+uint pixelLightCount = GetAdditionalLightsCount();
+  
+InputData inputData = (InputData)0;
+inputData.positionWS = Position;
+inputData.normalWS = s.normal;
+inputData.viewDirectionWS = s.view;
+inputData.shadowCoord = shadowCoord;
+    
+float4 screenPos = float4(s.clipPosition.x, (_ScaledScreenParams.y - s.clipPosition.y), 0, 0);
+inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(screenPos);
+    
+LIGHT_LOOP_BEGIN(pixelLightCount)
+    Light addLight = GetAdditionalLight(lightIndex, Position);
+    Color += CalculateCelShading(addLight, s);
+LIGHT_LOOP_END
+
+#endif
    
 #endif
 }
